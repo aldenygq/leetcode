30bce1db09fb663b290770ca34e3eb76b44f0fb2
guoqiang0114 <ygq13572549874@163.com>
2022-11-15 17:00:33 +0800

    update

--- isPalindrome.go
+++ isPalindrome.go
@@ -1,4 +1,9 @@
 package main
+
+import (
+	"fmt"
+)
+
 func isPalindrome(x int) bool {
 	// 特殊情况：
 	// 如上所述，当 x < 0 时，x 不是回文数。
@@ -13,10 +18,17 @@ func isPalindrome(x int) bool {
 	for x > revertedNumber {
 		revertedNumber = revertedNumber * 10 + x % 10
 		x /= 10
+		fmt.Printf("x:%v\n",x)
+		fmt.Printf("revertedNumber:%v\n",revertedNumber)
 	}
 	
 	// 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
 	// 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
 	// 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
 	return x == revertedNumber || x == revertedNumber / 10
+}
+
+func main(){
+	a := 1221
+	fmt.Printf("a:%v\n",isPalindrome(a))
 }
